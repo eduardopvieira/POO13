eb79ce2f4005cb63c310880aa49038f1b894ee94
ErkPatrick <116852792+ErkPatrick@users.noreply.github.com>
2023-09-17 18:12:56 -0300

    Update Orcamento.java

--- br/edu/ufersa/Oficina/src/model/entity/Orcamento.java
+++ br/edu/ufersa/Oficina/src/model/entity/Orcamento.java
@@ -96,3 +96,117 @@ public int getIdOrcamento() {return idOrcamento;}
     }
   }
 }
+
+
+
+
+
+
+
+
+=================================SEGUNDA OPCAO =======================================
+
+
+package entity;
+
+import java.util.Date;
+
+import exceptions.InformacaoInvalida;
+
+public class Orcamento {
+
+  private int idOrcamento;
+  private Cliente clienteOrcamento;
+  private Automovel automovelOrcamento;
+  private Pecas pecasOrcamento;
+  private Servico servicoOrcamento;
+  private double totalOrcamento;
+  private Date dataOrcamento;
+
+  public Orcamento(){}
+  public Orcamento(Cliente clienteOrcamento, Automovel automovelOrcamento, Pecas pecasOrcamento, Servico servicoOrcamento) throws InformacaoInvalida{
+      
+      setClienteOrcamento(clienteOrcamento);
+      setAutomovelOrcamento(automovelOrcamento);
+      setPecasOrcamento(pecasOrcamento);
+      setServicoOrcamento(servicoOrcamento);
+      setDataOrcamento(dataOrcamento);
+      setTotalOrcamento(totalOrcamento);
+  }
+
+
+	public void setDataOrcamento(Date data) {
+	  this.dataOrcamento = data;
+	}
+	
+	public Date getDataOrcamento() {
+	    return dataOrcamento;
+	}
+	
+	
+	public int getIdOrcamento() {return idOrcamento;}
+	
+	/*==>GET/SET clienteOrcamento<==*/
+	  public Cliente getClienteOrcamento(){
+	    return clienteOrcamento;
+	  }
+	  public void setClienteOrcamento(Cliente clienteOrcamento){
+	    this.clienteOrcamento = clienteOrcamento;
+	  }
+	/*<========================>*/
+	
+	/*==>GET/SET automovelCliente<==*/
+	  public Automovel getAutomovelOrcamento(){
+	    return automovelOrcamento;
+	  }
+	  public void setAutomovelOrcamento(Automovel automovelOrcamento){
+	    this.automovelOrcamento = automovelOrcamento;
+	    System.out.println("Automovel atribuido.");
+	  }
+	/*<========================>*/
+	
+	/*==>GET/SET pecasOrcamento<==*/
+	  public Pecas getPecasOrcamento(){
+	    return pecasOrcamento;
+	  }
+	  public void setPecasOrcamento(Pecas pecasOrcamento){
+	    this.pecasOrcamento = pecasOrcamento;
+	    System.out.println("Pecas atribuidos.");
+	  }
+	/*<========================>*/
+	
+	/*==>GET/SET<==*/
+	  public Servico getServicoOrcamento(){
+	    return servicoOrcamento;
+	  }
+	  public void setServicoOrcamento(Servico servicoOrcamento){
+	    this.servicoOrcamento = servicoOrcamento;
+	    System.out.println("Servicos atribuidos.");
+	  }
+	/*<========================>*/
+	
+	/*==>METODO ALTERAR CLIENTE<==*/
+	  public void alteraOrcamento(){
+	    System.out.println("Editando Orcamento");
+	  }
+	/*<========================>*/
+	
+	/*METODO EXCLUIR CLIENTE(ESQUECER)*/
+	  public void excluiOrcamento(){
+	    System.out.println("Confirma exclusao do orcamento? ");
+	  }
+	/*<========================>*/
+	/*GET E SET PREÇO TOTAL DO ORÇAMENTO*/
+	  public double getTotalOrcamento() {
+	      return totalOrcamento;
+	  }
+	
+	  public void setTotalOrcamento(double totalOrcamento) throws InformacaoInvalida {
+	    if (totalOrcamento >= 0) {  
+	    	this.totalOrcamento = totalOrcamento;
+	    }
+	    else 
+	    	throw new InformacaoInvalida("O valor do orçamento não pode ser negativo");
+	 }
+}
+
