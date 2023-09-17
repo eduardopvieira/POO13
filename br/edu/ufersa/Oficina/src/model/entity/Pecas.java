ffb03ebbd908b47f29891c3ea2bdfec59ac2579d
ErkPatrick <116852792+ErkPatrick@users.noreply.github.com>
2023-09-17 18:13:41 -0300

    Update Pecas.java

--- br/edu/ufersa/Oficina/src/model/entity/Pecas.java
+++ br/edu/ufersa/Oficina/src/model/entity/Pecas.java
@@ -76,3 +76,82 @@ public class Pecas {
 		this.unidadeMedidaItem = unidadeMedidaItem;}
 	}
 }
+
+
+
+
+
+
+
+
+
+==============================SEGUNDA OPCAO ==========================================
+
+
+package entity;
+import exceptions.InformacaoInvalida;
+
+public class Pecas {
+	private int idItem;
+	private String descricaoItem;
+	private double precoItem;
+	private int estoqueItem;
+	private int unidadeMedidaItem;
+	private String fabricante;
+	
+	//========= construtores ===============
+	
+	public Pecas() {}
+	public Pecas(String descricaoItem, double precoItem, int estoqueItem, int unidadeMedidaItem, String fabricante) throws InformacaoInvalida{
+		setDescricaoItem(descricaoItem);
+		setPrecoItem(precoItem);
+		setEstoqueItem(estoqueItem);
+		setUnidadeMedidaItem(unidadeMedidaItem);
+		setFabricante(fabricante);
+	}
+	public String getDescricaoItem() {
+		return this.descricaoItem;
+	}
+	public void setDescricaoItem(String descricaoItem) throws InformacaoInvalida {
+		if(descricaoItem != null && !descricaoItem.isEmpty())
+			this.descricaoItem = descricaoItem;
+		else
+			throw new InformacaoInvalida("Descrição inválida");
+	}
+	public double getPrecoItem() {
+		return this.precoItem;
+	}
+	public void setPrecoItem(double precoItem) throws InformacaoInvalida{
+		if(precoItem >= 0)
+			this.precoItem = precoItem;
+		else
+			throw new InformacaoInvalida("O preço de um produto não pode ser negativo");
+	}
+	public int getEstoqueItem() {
+		return this.estoqueItem;
+	}
+	public void setEstoqueItem(int estoqueItem) throws InformacaoInvalida{
+		if(estoqueItem >= 0)
+			this.estoqueItem = estoqueItem;
+		else
+			throw new InformacaoInvalida("O estoque não pode ser negativo");
+	}
+	public int getUnidadeMedidaItem() {
+		return this.unidadeMedidaItem;
+	}
+	public void setUnidadeMedidaItem(int unidadeMedidaItem) throws InformacaoInvalida{
+		if(unidadeMedidaItem >= 0)
+			this.unidadeMedidaItem = unidadeMedidaItem;
+		else
+			throw new InformacaoInvalida("Uma item não pode ter uma unidade de medida negativa");
+	}
+	public String getFabricante() {
+		return this.fabricante;
+	}
+	public void setFabricante(String fabricante) throws InformacaoInvalida {
+		if(fabricante != null && !fabricante.isEmpty())
+			this.fabricante = fabricante;
+		else
+			throw new InformacaoInvalida("O fabricante não pode ser vazio");
+	}
+}
