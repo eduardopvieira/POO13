8685775065b5ee65c1a392a4e954655852182648
ErkPatrick <116852792+ErkPatrick@users.noreply.github.com>
2023-09-17 18:10:27 -0300

    Update Cliente.java

--- br/edu/ufersa/Oficina/src/model/entity/Cliente.java
+++ br/edu/ufersa/Oficina/src/model/entity/Cliente.java
@@ -22,3 +22,47 @@ public class Cliente extends Pessoa{
     }
 
 }
+
+
+
+
+========================================SEGUNDA OPCAO =====================================
+
+package entity;
+
+import exceptions.InformacaoInvalida;
+import java.util.ArrayList;
+
+public class Cliente extends Pessoa{
+	private Automovel[] automovel = new Automovel[10];
+	private ArrayList<Automovel> automoveis = new ArrayList<Automovel>();
+
+	
+	
+	
+	// =========== construtores =========
+	public Cliente() {};
+	
+	public Cliente(String nome, String cpf, String endereco) throws InformacaoInvalida {
+		super(nome, cpf, endereco);
+	}
+	
+	public Cliente(String nome, String cpf, String endereco, Automovel []automovel) throws InformacaoInvalida{
+		super(nome, cpf, endereco);
+		setAutomovel(automovel);
+	}
+	
+	//===================== get e set =======================
+	public Automovel[] getAutomovel() {
+		return this.automovel;
+	}
+
+	public void setAutomovel(Automovel[] automovel) throws InformacaoInvalida {
+		for(int i = 0; i <10; i++) {
+			if(automovel[i] != null)
+				this.automoveis.add(automovel[i]);
+			else
+				throw new InformacaoInvalida("");
+		}
+	}
+}
